90662255be6afcac6ccd9f31c4984803c1b4a16a
seasign10 <seasign10@gmail.com>
2025-05-29 15:31:40 +0900

    add: backend

---
+++ backend/src/test/java/com/bob/smash/SmashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob.smash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
