5a2b51edab8dffa6a8a701f2ed4cc919c43c9125
syundo <syundo@outlook.kr>
2025-06-04 16:08:36 +0900

    edit: Estimate

--- backend/src/main/java/com/bob/smash/controller/smashController.java
+++ backend/src/main/java/com/bob/smash/controller/smashController.java
@@ -18,7 +18,7 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/smash")
 // @RequiredArgsConstructor // 자동주입
 @Log4j2
-public class smashController {
+public class SmashController {
   // service 주입
 
   // application.properties에서 Frontend URL 설정
--- backend/src/main/java/com/bob/smash/repository/estimateRepository.java
+++ backend/src/main/java/com/bob/smash/repository/estimateRepository.java
@@ -3,6 +3,6 @@ package com.bob.smash.repository;
 import com.bob.smash.entity.Estimate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface estimateRepository extends JpaRepository<Estimate, Integer> {
+public interface EstimateRepository extends JpaRepository<Estimate, Integer> {
   
 }
