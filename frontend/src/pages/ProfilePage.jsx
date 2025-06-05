260a0ca5e9cc43b3405094a3a739eca61d2498fe
sm <sumin599421@gmail.com>
2025-06-05 15:10:39 +0900

    feat: User, Partner 마이페이지 UI 구조

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,12 +1,13 @@
-import Login from './member/LoginPage';
-import Member from './MemberPage';
+// import Login from './member/LoginPage';
+import Member from './profile/UserProfile';
 
 const Profile = () => {
   const user = JSON.parse(localStorage.getItem('user'));
 
   return (
     <div className="profile">
-      {!user ? <Login /> : <Member />}
+      {/* {!user ? <Login /> : <Member />} */}
+      <Member />
     </div>
   );
 }
---
+++ frontend/src/pages/profile/PartnerProfile.jsx
@@ -0,0 +1,45 @@
+
+
+const PartnerProfile = () => {
+
+    return(
+        <div className="member-page">
+            <div className="member_main_container">
+                <h1>마이페이지</h1>
+                <div className="member_inform">
+                    <div className="inform_img">
+                        {/* 이미지 파일 작성 <img src={info.profileImage} alt="프로필" /> */}
+                    </div>
+                    <div className="inform_text">
+                        {/* 사업체 닉네임, 로그인 타입 출력 */}
+                    </div>
+                </div>
+
+                <div className="partner_infom">
+                    <h2>업체명</h2>
+                    <h2>사업자 번호</h2>
+                </div>
+
+                <section className="account_setting">
+                    <h2>SMaSh 이용 내역</h2>
+                    <ul>
+                        <li>회사 소개</li>
+                        <li>보낸 견적서</li>
+                        <li>작성된 리뷰</li>
+                    </ul>
+                </section>
+
+                <section className="account_setting">
+                    <h2>계정 설정</h2>
+                    <ul>
+                        <li>개인 정보 수정</li>
+                        <li>로그아웃</li>
+                        <li>계정 탈퇴</li>
+                    </ul>
+                </section>
+            </div>
+        </div>
+    );
+}
+
+export default PartnerProfile;
