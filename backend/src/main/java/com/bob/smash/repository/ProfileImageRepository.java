b7a692adcd66b7ac4753ab9cfe7e1b7b35995757
sm <sumin599421@gmail.com>
2025-06-05 17:15:37 +0900

    fix: ProfileImageRepository 오류 Member->Long 으로 수정

--- backend/src/main/java/com/bob/smash/repository/ProfileImageRepository.java
+++ backend/src/main/java/com/bob/smash/repository/ProfileImageRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ProfileImageRepository extends JpaRepository <ProfileImage, Member> {
+public interface ProfileImageRepository extends JpaRepository <ProfileImage, Long> {
     Optional<ProfileImage> findByMember(Member member);
 }
