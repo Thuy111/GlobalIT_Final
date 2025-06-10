99c08ad964af028d48ec1db45b1cb4b4eda46edd
seasign10 <seasign10@gmail.com>
2025-06-10 15:22:04 +0900

    feat: 소셜로그인

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,13 +1,36 @@
+import { useState, useEffect } from 'react';
 import Login from './member/LoginPage';
 import Member from './profile/UserProfile';
+import axios from 'axios';
 
 const Profile = () => {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const baseUrl = import.meta.env.VITE_API_URL;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // 로그인 상태 확인
+    const checkLoginStatus = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/smash/member/user`, { withCredentials: true });
+        if (response.data) {
+          // 유저정보
+          console.log('User is logged in:', response.data);
+          setIsLoggedIn(true);
+        } else {
+          console.log('User is not logged in');
+          setIsLoggedIn(false);
+        }
+      } catch (error) {
+        console.error('Error checking login status:', error);
+        setIsLoggedIn(false);
+      }
+    };
+    checkLoginStatus();
+  }, []);
 
   return (
     <div className="profile">
-      {!user ? <Login /> : <Member />}
-      {/* <Member /> */}
+      {isLoggedIn ? <Member /> : <Login />}
     </div>
   );
 }
