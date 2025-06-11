72247b698142d7b76733874bacedd5d588433d88
seasign10 <seasign10@gmail.com>
2025-06-11 13:54:07 +0900

    fix: 인증요청 페이지 수정

---
+++ backend/src/main/java/com/bob/smash/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.bob.smash.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("")
+    public String home() {
+        return "redirect:/smash/"; 
+    }
+}
