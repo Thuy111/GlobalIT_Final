8261ecb8e9cb3cba80d099a8a0b6a42fea6d86a6
sm <sumin599421@gmail.com>
2025-06-13 17:17:09 +0900

    feat : 구글 프로필 이미지

--- backend/src/main/java/com/bob/smash/repository/ProfileImageRepository.java
+++ backend/src/main/java/com/bob/smash/repository/ProfileImageRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProfileImageRepository extends JpaRepository <ProfileImage, String> {
-    Optional<ProfileImage> findByMember(Member member);
+    Optional<ProfileImage> findByMember(Member member); // 회원에 대한 프로필 이미지 찾기
 }
--- backend/src/main/java/com/bob/smash/service/ProfileServiceImpl.java
+++ backend/src/main/java/com/bob/smash/service/ProfileServiceImpl.java
@@ -19,21 +19,23 @@ public class ProfileServiceImpl implements ProfileService{
 
     @Override
     public ProfileDTO getProfileByEmail (String emailId){
-        Member member = memberRepository.findById(emailId)
+        // 이메일로 회원 정보 조회
+        Member member = memberRepository.findByEmailId(emailId)
                         .orElseThrow(() -> {
                             log.warn("회원 정보 조회 실패 - 존재하지 않는 emailId: {}", emailId);
                             return new IllegalArgumentException("회원이 존재하지 않습니다.");
     });
         
+    // 파트너 정보 여부 확인
         boolean isPartner = partnerInfoRepository.findByMember_EmailId(emailId).isPresent();
 
+        // 프로필 이미지 조회
         Optional<ProfileImage> profileOpt = profileImageRepository.findById(member.getEmailId());
-
         String profileImageUrl = profileOpt
                 .map(img -> img.getPath() + "/" + img.getSName())
-                .orElse(null);
+                .orElse(null); 
 
-
+        // ProfileDTO 생성 및 반환
         return  ProfileDTO.builder()
                     .email(member.getEmailId())
                     .nickname(member.getNickname())
