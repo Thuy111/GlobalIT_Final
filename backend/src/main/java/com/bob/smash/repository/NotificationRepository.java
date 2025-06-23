ed2d912c550e47d1e61ee4427eeb34da56077db9
syundo <syundo@outlook.kr>
2025-06-23 15:45:21 +0900

    fix: repo 네이밍 오류 해결

--- backend/src/main/java/com/bob/smash/repository/NotificationRepository.java
+++ backend/src/main/java/com/bob/smash/repository/NotificationRepository.java
@@ -8,7 +8,7 @@ import com.bob.smash.entity.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     // 알림 조회: 회원 ID로 알림 목록 조회
-    List<Notification> findByMemberId(String memberId);
-    // 특정 회원의 읽지 않은 알림 개수 조회
-    long countByMemberIdAndIsReadFalse(String memberId);
+    List<Notification> findByMember_EmailId(String emailId);
+    // 특정 회원의 읽지 않은/읽은 알림 개수 조회(isRead가 0: 읽지 않음)
+    long countByMember_EmailIdAndIsRead(String emailId, Byte isRead);
 }
