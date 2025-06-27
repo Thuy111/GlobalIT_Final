abd9edb33e3e3e313eff9be464ad15f556121cce
cksdud0 <cksdud0.com>
2025-06-27 11:28:51 +0900

    리뷰평균점수 추가

--- backend/src/main/java/com/bob/smash/repository/ReviewRepository.java
+++ backend/src/main/java/com/bob/smash/repository/ReviewRepository.java
@@ -11,4 +11,6 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
   void deleteByEstimate_Idx(Integer idx); // 견적서 번호로 리뷰 전체 삭제
   List<Review> findByEstimate_Idx(Integer estimateIdx); // 견적서 번호맞는 리뷰
   List<Review> findByMember_EmailId(String email);
+
+  
 }
